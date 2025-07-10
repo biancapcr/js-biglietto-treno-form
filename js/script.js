f1dc61886cb477b0889f01ae50ce9f43afff3cba
biancapcr <biancacristina.pocora@gmail.com>
2025-07-10 17:19:25 +0200

    [add] ticketcontainer and [add] carrozza-codice-cp + ageCat

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 
 document.getElementById('genera').addEventListener('click', function() {
   const km  = parseFloat(document.getElementById('km').value);
-  const age = parseInt(document.getElementById('age').value, 10);
+  const ageCat = document.getElementById('age').value;
+  const ticketContainer = document.getElementById('ticket');
 
   // Calcola prezzo base
   let price = km * 0.21;
@@ -17,6 +18,21 @@ document.getElementById('genera').addEventListener('click', function() {
     price *= 0.6;   
   }
 
-  // output
-  console.log(`Prezzo finale del biglietto: €${price.toFixed(2)}`);
+ // Formattazione
+  const formattedPrice = price.toFixed(2) + '€';
+
+ // 5) Età
+  let ageText;
+  if (ageCat === 'minorenne')       
+    ageText = 'Minorenne';
+  else if (ageCat === 'over65')     
+    ageText = 'Over 65';
+  else                               
+  ageText = 'Maggiorenne';
+
+   // Carrozza e codice CP
+  const carriage = Math.floor(Math.random() * 9) + 1;
+  const code = Math.floor(Math.random() * 90000) + 10000;
+
+
 });
