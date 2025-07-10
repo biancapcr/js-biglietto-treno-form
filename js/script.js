b4f5b300638ab41ac15e6ea28d9ea13655f4e286
biancapcr <biancacristina.pocora@gmail.com>
2025-07-10 17:42:11 +0200

    [add] innerhtml

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-// Milestone 1: calcolo prezzo su due input, output in console
 
 document.getElementById('genera').addEventListener('click', function() {
   const km  = parseFloat(document.getElementById('km').value);
+  const name = document.getElementById('name').value;
   const ageCat = document.getElementById('age').value;
   const ticketContainer = document.getElementById('ticket');
 
   // Calcola prezzo base
   let price = km * 0.21;
 
-  // Applicazione dello sconto
-  if (age < 18) {
-    // 20% di sconto
-    price *= 0.8;   
+  // 3) Applicazione sconto
+  let offerText = 'Biglietto Standard';
+  if (ageCat === 'minorenne') {
+    price *= 0.8;
+    offerText = 'Sconto Minorenni';
   } 
-  else if (age >= 65) {
-    // 40% di sconto
-    price *= 0.6;   
+  else if (ageCat === 'over65') {
+    price *= 0.6;
+    offerText = 'Sconto Over 65';
   }
-
- // Formattazione
+  // Formattazione
   const formattedPrice = price.toFixed(2) + '€';
 
- // 5) Età
-  let ageText;
-  if (ageCat === 'minorenne')       
-    ageText = 'Minorenne';
-  else if (ageCat === 'over65')     
-    ageText = 'Over 65';
-  else                               
-  ageText = 'Maggiorenne';
-
-   // Carrozza e codice CP
+  // Carrozza e codice CP
   const carriage = Math.floor(Math.random() * 9) + 1;
   const code = Math.floor(Math.random() * 90000) + 10000;
 
+  // 7) Iniezione in pagina
+   document.getElementById('ticket').innerHTML = `<div class="ticket">
+      <h2>Il tuo biglietto</h2>
+      <div><strong>Dettaglio Passeggero:</strong> ${name}</div>
+      <p><strong>Offerta:</strong> ${offerText}</p>
+      <div><strong>Carrozza:</strong> ${carriage}</div>
+      <div><strong>Codice CP:</strong> ${code}</div>
+      <div><strong>Costo biglietto:</strong> ${formattedPrice}</div>
+    </div>`;
 
 });
